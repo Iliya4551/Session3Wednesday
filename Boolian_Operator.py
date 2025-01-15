9963eb5369819bceb45bedb308853120eca3c6f5
iliyachaaban <ichaaban.ieu2023@student.ie.edu>
2025-01-15 18:52:53 +0100

    Added arithmetic and boolean

---
+++ Boolian_Operator.py
@@ -0,0 +1,8 @@
+print(not True)
+print(not False) #when you see not, it either produces true or false
+print (not 7) #it is false because 7 is true (it's there)
+
+print(True or False)
+print(True and False)
+print(2 or 3 or 4) #for "or" it always prints the first true
+print(1 and 2 and 3) #for "and" it always prints the last true
---
+++ arithmetic operators.py
@@ -0,0 +1,7 @@
+print(2+6)
+print(9-99)
+print(3*8)
+print(20/5) #always produces float result (4.0 not 4)
+print(20//5) #this is the integer division (4 not 4.0)
+print(22%5) #it gives you the remainder
+print(2**3) #2 stars makes it an exponent
